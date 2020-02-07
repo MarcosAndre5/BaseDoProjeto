60c1949729f8148ccc20a10fa9ca46855ce46e16
MarcosAndre5 <marcosandreazevedo@hotmail.com>
2020-02-07 17:22:20 -0300

    Finalizando capitulo 4 do Livo.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,14 +5,18 @@ namespace estoque\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 
 class ProdutoController extends Controller{
-	public function lista(){
-		$html = '<h1>Listagem de produtos com Laravel.</h1>';
-		$html .= '<ul>';
-		$produtos = DB::select('SELECT * FROM produtos');
-		foreach($produtos as $p){
-			$html.='<li>Nome: '.$p->nome.', Descriçao: '.$p->descricao.'</li>';
-		}
-		$html .= '</ul>';
-		return $html;
-	}
+    public function lista(){
+        $html = '<h1>Listagem de produtos com Laravel.</h1>';
+        $html .= '<ul>';
+
+        $produtos = DB::select('SELECT * FROM produtos');
+
+        foreach($produtos as $p){
+            $html.='<li><b>Nome:</b> '.$p->nome.'<br><b>Descriçao:</b> '.$p->descricao.'</li><br>';
+        }
+
+        $html .= '</ul>';
+
+        return $html;
+    }
 }
