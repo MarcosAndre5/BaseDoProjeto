a234bb3dbaa3177ff06fddcd6b0ef2aa3ef0c7d4
MarcosAndre5 <marcosandreazevedo@hotmail.com>
2020-05-05 19:11:52 -0300

    Parte 8 finalizada.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,19 +3,29 @@
 namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use Request;
+use Illuminate\Http\Request;
 
 class ProdutoController extends Controller{
-    public function lista(){
+    public function listarProdutos(){
         $produtos = DB::select('SELECT * FROM produtos');
         return view('produto.listagem')->with('produtos', $produtos);
     }
 
-    public function mostra($id){
+    public function visualizarProduto($id){
         $produto = DB::select('SELECT * FROM produtos WHERE id = ?', [$id]);
         if(empty($produto)){
             return "Esse produto não existe.";
         }
         return view('produto.detalhes')->with('produto', $produto);
+    }
+
+    public function novoProduto(){
+        return view('produto.formulario');
+    }
+
+    public function cadastrarProduto(Request $request){
+        $produto = $request->all();
+        DB::insert('INSERT INTO produtos (nome, descricao, valor, quantidade) VALUES (?, ?, ?, ?)', array($produto['nome'], $produto['descricao'], $produto['valor'], $produto['quantidade']));
+        return view('produto.adicionado')->with('nome', $produto['nome']);
     }
 }
